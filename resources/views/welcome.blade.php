0a67422c964c1b96559774dcdfc410ca63329b54
Jaison Neira <estudiosjaison@gmail.com>
2022-03-07 09:59:40 -0500

    cambios pequeños

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,7 +34,7 @@
           <a class="nav-link" aria-current="page" href="#">Tablas</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Agregar producto</a>
+          <a class="nav-link" href="#">Agregar aprendiz</a>
         </li>
       </ul>
     </div>
@@ -53,7 +53,7 @@
            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
               <h2>Tabla Aprendices</h2>
               <div>
-
+              <label>Suma Edades:</label>
               <input class="form-control" type="text" placeholder="" value="{{$suma}}" aria-label="Disabled input example" disabled>
               </div>
               
